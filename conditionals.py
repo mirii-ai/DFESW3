902b0b94768dc11cbc3ed4b0fbc6bf69cfe059ae
mirii-ai <56605032+mirii-ai@users.noreply.github.com>
2021-10-04 13:14:55 +0100

    New conditional changes (while)

--- conditionals.py
+++ conditionals.py
@@ -9,19 +9,31 @@
 
 # print("this line will always print")
 
-grade = int(input("Please type the mark of the test." ))
+# grade = int(input("Please type the mark of the test." ))
 
-if grade > 85:
-    print("DISTINCTION")
-elif grade >= 65 and grade <= 85:
-    print("PASS")
-else:
-    print("FAIL")
+# if grade > 85:
+#     print("DISTINCTION")
+# elif grade >= 65 and grade <= 85:
+#     print("PASS")
+# else:
+#     print("FAIL")
 
 
-if grade > 85:
-    print("DISTINCTION")
-if grade >= 65 and grade <= 85:
-    print("PASS")
-if grade < 65:
-    print("FAIL")
+# if grade > 85:
+#     print("DISTINCTION")
+# if grade >= 65 and grade <= 85:
+#     print("PASS")
+# if grade < 65:
+#     print("FAIL")
+
+inputNumVar = int(input("Type a whole number please. "))
+while inputNumVar > 0:
+    print(inputNumVar)
+    inputNumVar = inputNumVar - 1
+
+numvar2 = int(input("Type another number please. "))
+resultVar = 1
+while numvar2 > 0:
+    resultVar = resultVar * numvar2
+    numvar2 = numvar2 - 1
+print(resultVar)

80ea0dc0c11f665d0c1f79a6fb30acfc7b79b6d3
mirii-ai <56605032+mirii-ai@users.noreply.github.com>
2021-10-05 11:26:46 +0100

    new function excercises

---
+++ functions and procedures.py
@@ -0,0 +1,110 @@
+# def add_calc(number1, number2):
+#     answer = number1 + number2
+#     return answer
+
+# added_number = add_calc(5,5)
+# print(added_number + 20)
+
+#---------------------------------------
+#---------------------------------------
+#QA
+
+# def grading(name, hw_score, assessment_score, exam_score):
+#     student_name = name
+#     total = 175
+#     student_total = hw_score + assessment_score + exam_score
+#     overall = (student_total/175) * 100
+
+#     return student_name, overall
+
+
+# studentname = input("Please type student name: ")
+# hw = int(input("Homework score: "))
+# asmnt = int(input("Assessment score: "))
+# exam = int(input("Exam score: "))
+# name, grade = grading(studentname, hw, asmnt, exam)
+# print("Student name : ", name, " Overall ICT grade : " , round(grade), "%")
+
+#---------------------------------------
+#---------------------------------------
+#W3RESOURCE
+#QUESTION1
+
+# def find_max(num1, num2, num3):
+#     list = [num1, num2, num3]
+
+#     new = sorted(list)
+
+#     print(new)
+#     return(new[-1])
+
+# number1 = int(input("Please type a number. "))
+# number2 = int(input("Please type a number. "))
+# number3 = int(input("Please type a number. "))
+# biggest = find_max(number1, number2, number3)
+
+# print("The max is: " + str(biggest))
+
+
+#---------------------------------------
+#---------------------------------------
+# Write a Python function to sum all the numbers in a list.
+# Sample List : (8, 2, 3, 0, 7)
+# Expected Output : 20
+
+# def sorted_list(list_name):
+#     func_list = list_name
+
+#     return_list = sorted(func_list)
+
+#     return(return_list)
+
+# times = int(input("How many items would you like to insert? "))
+# input_list = []
+
+# for i in range(times):
+#     number = int(input("Please type a number you'd like to input. "))
+#     input_list.append(number)
+
+
+
+# print("SORTED LIST: ")
+# finished_list = sorted_list(input_list)
+# print(finished_list)
+
+
+#---------------------------------------
+#---------------------------------------
+
+
+
+
+#---------------------------------------
+#---------------------------------------
+
+
+
+
+#---------------------------------------
+#---------------------------------------
+
+
+
+
+
+#---------------------------------------
+#---------------------------------------
+
+
+
+
+
+#---------------------------------------
+#---------------------------------------
+
+
+
+
+
+#---------------------------------------
+#---------------------------------------
